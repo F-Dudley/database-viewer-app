c4584619b8ca8cec8482f7bcee290401262b1f95
F_Dudley4 <50776815+F-Dudley@users.noreply.github.com>
2021-08-17 01:30:08 +0100

    Initial Push
    Initial Push of Project with Electron, React, SCSS and Webpack Set-up.

---
+++ src/App/index.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import './App.scss';
+
+const App = () => {
+    return (
+        <>
+            <h1 className="test">I Work</h1>
+        </>
+    )
+}
+
+export default App
+
